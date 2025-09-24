0c20915bbd97ecfdfb6dc0ab1f3ffd6bc278d764
Flavio Garcia <piraz@candango.org>
2025-09-24 00:33:05 -0400

    feat(cli): introduce ecdh decrypt command
    Fixes: #8

--- internal/cli/commands.go
+++ internal/cli/commands.go
@@ -36,8 +36,9 @@ var keymanagerConstructors = map[string]func(priv *ecdh.PrivateKey) ec.EcdhKeyMa
 }
 
 type EcdhCommand struct {
-	Generate EcdhGenCommand   `command:"generate" description:"Generate EC Key"`
-	Encrypt  EcEncryptCommand `command:"encrypt" description:"Encrypt a value using an EC Key"`
+	Generate EcdhGenCommand     `command:"generate" description:"Generate ECDH Key"`
+	Encrypt  EcdhEncryptCommand `command:"encrypt" description:"Encrypt a value using an ECDH Key"`
+	Decrypt  EcdhDecryptCommand `command:"decrypt" description:"Decrypt a secret using an ECDH Key"`
 }
 
 type EcdhGenCommand struct {
@@ -118,12 +119,12 @@ type EcdhEncryptPositionalArgs struct {
 	Text string `description:"Text to be encrypted"`
 }
 
-type EcEncryptCommand struct {
+type EcdhEncryptCommand struct {
 	Key  string                    `short:"k" long:"key" description:"Private key file to be used for encryption"`
 	Args EcdhEncryptPositionalArgs `positional-args:"yes"`
 }
 
-func (cmd *EcEncryptCommand) Execute(args []string) error {
+func (cmd *EcdhEncryptCommand) Execute(args []string) error {
 	if cmd.Key == "" {
 		return errors.New("empty Key file. Please inform the Private Key file to be used")
 	}
@@ -186,6 +187,87 @@ func (cmd *EcEncryptCommand) Execute(args []string) error {
 	result := base64.StdEncoding.EncodeToString([]byte(secret))
 
 	fmt.Println(result)
+
+	return nil
+}
+
+type EcdhDecryptPositionalArgs struct {
+	Secret string `description:"Secret to be decrypted"`
+}
+
+type EcdhDecryptCommand struct {
+	Key  string                    `short:"k" long:"key" description:"Private key file to be used for decryption"`
+	Args EcdhDecryptPositionalArgs `positional-args:"yes"`
+}
+
+func (cmd *EcdhDecryptCommand) Execute(args []string) error {
+	if cmd.Key == "" {
+		return errors.New("empty Key file. Please inform the Private Key file to be used")
+	}
+
+	fi, err := os.Stdin.Stat()
+	if err != nil {
+		return err
+	}
+
+	secret := cmd.Args.Secret
+	if (fi.Mode() & os.ModeCharDevice) == 0 {
+		input, err := io.ReadAll(os.Stdin)
+		if err != nil {
+			return err
+		}
+		secret = string(input)
+	}
+
+	if secret == "" {
+		return errors.New("empty Secret. Please infor a Secret to be decrypted")
+	}
+
+	_, err = os.Stat(cmd.Key)
+
+	if err != nil {
+		return err
+	}
+
+	data, err := os.ReadFile(cmd.Key)
+	if err != nil {
+		return err
+	}
+
+	pk, err := ec.EcdhP256PemToPrivateKey(data)
+	if err != nil {
+		return err
+	}
+	curve := strings.ReplaceAll(fmt.Sprintf("%s", pk.Curve()), "-", "")
+	keymanagerConstructor, ok := keymanagerConstructors[curve]
+	if !ok {
+		return fmt.Errorf("key manager for the curve %s was not found", curve)
+	}
+
+	keymanager := keymanagerConstructor(pk)
+	sharedSecret, err := keymanager.DeriveSharedSecret(pk.PublicKey())
+	if err != nil {
+		return err
+	}
+	nonce, err := symmetric.NewAESGCMNonce()
+	if err != nil {
+		return err
+	}
+	packed, err := base64.StdEncoding.DecodeString(secret)
+	if err != nil {
+		return err
+	}
+
+	ciphertext, nonce, err := pack.BigEndianSeparate(packed)
+	if err != nil {
+		return err
+	}
+	plain, err := symmetric.DecryptAESGCM(sharedSecret, nonce, ciphertext)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(string(plain))
 
 	return nil
 }
